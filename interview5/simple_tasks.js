a67e6bcdda6f542ed422edb6e9a4871a0c633f17
Louiks <kokot.jakub@wp.pl>
2022-07-26 18:30:25 +0200

    interview 5

--- interview5/simple_tasks.js
+++ interview5/simple_tasks.js
@@ -2,16 +2,16 @@
 !!"some string in here" //true
 !!0 //false
 !!undefined//false
-!![] //false
+!![] //true
 !!{} //true
 !!NaN //false
-typeof null;   //undefined
-typeof undefined; //undefined
-typeof NaN; //Error
-typeof typeof 1; //
-typeof [];  //Array<any>
-NaN === NaN; //true
-NaN == NaN; //true
+typeof null;   //'object'
+typeof undefined; //'undefined'
+typeof NaN; //'object'
+typeof typeof 1; //'string'
+typeof [];  //'object'
+NaN === NaN; //false
+NaN == NaN; //false
 console.log(1 < 2 < 3); //true
 console.log(3 > 2 > 1); //false
 true + false //1
@@ -21,20 +21,12 @@ true + false //1
  * Find min and max values in array of numbers
  */
 
-export function findMinMax(array: number[]): { min: number, max: number } {
-	// write code here
-  let min = array[0];
-  let max = array[0];
-  for (let i in array) {
-    min = min < i ? i : min;
-    max = max > i ? i : max;
-  }
+export function findMinMax(array): { min: number, max: number } {
   return {
-    min: min,
-    max: max,
+    min: Math.min(...array),
+    max: Math.max(...array),
   };
 }
-
 
 
 /*
@@ -47,8 +39,8 @@ export function findMinMax(array: number[]): { min: number, max: number } {
 
 export function isPalindrome(text: string): boolean {
   // write code here
-  for (let i = 0; i < Math.floor(text.length/2): i++) {
-     if (text[i] !== text[text.length-i]) return false;
+  for (let i = 0; i < Math.floor(text.length/2); i++) {
+     if (text[i] !== text[text.length-i-1]) return false;
   }
   return true;
 }
